57c71bcd97f0f902356f63d2cf559e339db62672
Ido Katz <ido.k@kelasys.com>
2025-12-01 09:14:54 +0200

    update android build script

--- build-android.sh
+++ build-android.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-env CGO_ENABLED=1 GOOS=android GOARCH=arm64 CC=~/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang ~/go1.24.3/go/bin/go build -ldflags "-checklinkname=0" -o binaries/libmediamtx.so  .
+NDK_PATH=~/Android/Sdk/ndk/27.0.12077973
+
+env CGO_ENABLED=1 GOOS=android GOARCH=arm64 CC=$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang go build -ldflags "-checklinkname=0" -o binaries/libmediamtx.so  .
